40661e7f9e776a585f8448dc578258f9168a6bc5
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2015-07-30 19:55:54 -0300

    Improve error reporting

--- src/nodejsevaluator.cc
+++ src/nodejsevaluator.cc
@@ -15,9 +15,22 @@ QVariant NodeJSEvaluator::eval(QString str)
 {
     HandleScope scope;
 
-    Local<Function> eval = Local<Function>::Cast( Context::GetCurrent()->Global()->Get(String::New("eval")) );
-    Local<Value> arguments[1] = { qt_v8::FromQString(str) };
-    Local<Value> result = eval->Call(Context::GetCurrent()->Global(), 1, arguments);
+    TryCatch trycatch;
 
-    return qt_v8::ValueToVariant(result);
+    Local<Script> script = Script::Compile(qt_v8::FromQString(str));
+	if (script.IsEmpty()) {
+        Local<Message> message = trycatch.Message();
+        qDebug() << qt_v8::ToQString(message->Get());
+	} else {
+        Local<Value> result = script->Run();
+
+        if (trycatch.HasCaught()) {
+            Local<Message> message = trycatch.Message();
+            qDebug() << qt_v8::ToQString(message->Get());
+        } else {
+            return qt_v8::ValueToVariant(result);
+        }
+    }
+
+    return QVariant();
 }
